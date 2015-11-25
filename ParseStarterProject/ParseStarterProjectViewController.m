9dc86bcf2fba265c2417531ef392431fba5654aa
Xiao Jiang <showbufire@gmail.com>
2015-11-25 15:42:29 -0800

    parse crash starter project

---
+++ ParseStarterProject/ParseStarterProjectViewController.m
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import "ParseStarterProjectViewController.h"
+
+#import <Parse/Parse.h>
+
+@implementation ParseStarterProjectViewController
+
+#pragma mark -
+#pragma mark UIViewController
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"Crash!" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(crash) forControlEvents:UIControlEventTouchUpInside];
+    button.center = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds));
+    [button sizeToFit];
+    [self.view addSubview:button];
+}
+
+- (void)crash {
+    [NSException raise:NSGenericException format:@"Everything is ok. This is just a test crash."];
+}
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
